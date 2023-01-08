56a776b5f8722d19c8c0cb5fbdb330e53701bc11
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-01-09 02:04:08 +0530

    updated file structure

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -5,6 +5,8 @@ function home() {
   return (
     <div>
 
+
+{/* fdasf */}
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -29,6 +31,11 @@ function home() {
 
 
 {/* ********************** why asme ***************************** */}
+
+
+
+
+
 
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
