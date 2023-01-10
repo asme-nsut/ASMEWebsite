ba62f1441f9c90bf17f6bc70205d7362af83933b
Chirag <chiragsaini6405@gmail.com>
2023-01-10 23:43:11 +0530

    commited at night

--- frontend/src/components/about/facultydiv.jsx
+++ frontend/src/components/about/facultydiv.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
-function facultydiv({items,facultyId}) {
-    console.log(facultyId);
-    const aligntext = (facultyId%2===0) ? 
-    <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-    <img alt="team" class="flex-shrink-0 rounded-full w-80 h-80 object-cover object-center sm:mb-0 mb-4" src={items.img} />
-    <div class="flex-grow sm:pl-8">
-      <h2 class="title-font font-medium text-lg text-gray-900">{items.name}</h2>
-      <h3 class="text-gray-500 mb-3">{items.por}</h3>
-      <p class="mb-4">{items.desc}</p>
-    </div>
-  </div> :
-  <div class="h-full flex sm:flex-row-reverse flex-col items-center sm:justify-start justify-center text-center sm:text-right">
-  <img alt="team" class="flex-shrink-0 rounded-full w-80 h-80 object-cover object-center sm:mb-0 mb-4" src={items.img} />
-  <div class="flex-grow sm:pr-8">
-    <h2 class="title-font font-medium text-lg text-gray-900">{items.name}</h2>
-    <h3 class="text-gray-500 mb-3">{items.por}</h3>
-    <p class="mb-4">{items.desc}</p>
-  </div>
-</div>;
-    
+AOS.init();
+
+function facultydiv({ items, facultyId }) {
+  console.log(facultyId);
+  const aligntext = (facultyId % 2 === 0) ?
+    <div data-aos="fade-right"  class="h-full m-5 flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+      <img alt="team" class="flex-shrink-0 rounded-full w-80 h-80 object-cover object-center sm:mb-0 mb-4" src={items.img} />
+      <div class="flex-grow sm:pl-8">
+        <h2 class="title-font font-medium text-2xl text-gray-900">{items.name}</h2>
+        <h3 class="text-blue-500 mb-3">{items.por}</h3>
+        <p class="mb-4">{items.desc}</p>
+      </div>
+    </div> :
+    <div data-aos="fade-left" class="h-full m-5 flex sm:flex-row-reverse flex-col items-center sm:justify-start justify-center text-center sm:text-right">
+      <img alt="team" class="flex-shrink-0 rounded-full w-80 h-80 object-cover object-center sm:mb-0 mb-4" src={items.img} />
+      <div class="flex-grow sm:pr-8">
+        <h2 class="title-font font-medium text-2xl text-gray-900">{items.name}</h2>
+        <h3 class="text-blue-500 mb-3">{items.por}</h3>
+        <p class="mb-4">{items.desc}</p>
+      </div>
+    </div>;
   return (
     <div class="px-28 pb-8">
-        {aligntext}
+      {aligntext}
     </div>
   )
 }
--- frontend/src/components/about/studentdiv.jsx
+++ frontend/src/components/about/studentdiv.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 function studentdiv({items,studentId}) {
   return (
-        <div class="p-4 px-14 lg:w-1/3 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src={items.img} />
+      <div data-aos="zoom-in" class="p-4 px-14 lg:w-1/3 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center p-5 mb-3" src={items.img} />
           <div class="w-full">
-            <h1 class="title-font font-medium text-lg text-gray-900">{items.name}</h1>
+            <h1 class="title-font font-medium text-xl text-gray-900">{items.name}</h1>
             <h3 class="text-gray-500 mb-3">{items.por}</h3>
           </div>
         </div>
