cebe74f1a3b609a3e8e492d9362ce1686f77a0bb
Chirag <chiragsaini6405@gmail.com>
2023-01-13 20:17:19 +0530

    a

--- frontend/src/components/about/facultydiv.jsx
+++ frontend/src/components/about/facultydiv.jsx
@@ -9,7 +9,7 @@ function facultydiv({ items, facultyId }) {
   const aligntext = (facultyId % 2 === 0) ?
     <div data-aos="fade-right"  class="h-auto  flex lg:flex-row  overflow-hidden flex-col items-center sm:justify-start justify-center text-left rounded-[50px] shadow-[5px_5px_20px_5px_#e2e8f0]  ">
       <img alt="team" class="flex-shrink-0 p-8 lg:p-0 rounded-[50px] lg:rounded-[10px] w-80 h-full max-h-[400px] object-cover object-center sm:mb-0" src={items.img} />
-      <div class="flex-grow sm:pl-8">
+      <div class="flex-grow pl-8 pr-8">
         <h2 class="title-font text-2xl text-gray-900 font-poppins font-semibold">{items.name}</h2>
         <h3 class="text-[#0C397D] font-poppins font-semibold mb-3">{items.por}</h3>
         <p class="mb-4 font-poppins font-medium mr-14">{items.desc}</p>
