c4055431e94091821dd757802fd2efd89d18aeab
Chirag <chiragsaini6405@gmail.com>
2023-01-13 23:36:56 +0530

    commit for student section elements

--- frontend/src/components/about/studentdiv.jsx
+++ frontend/src/components/about/studentdiv.jsx
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 function studentdiv({items,studentId}) {
   return (
       <div data-aos="zoom-in" class="lg:p-5 md:px-4 w-[100%] p-3 xl:w-1/3 md:w-1/2 mb-7">
-        <div class=" h-96 hover:bg-gradient-to-b hover:from-indigo-600 hover:via-indigo-400 hover:to-indigo-100 justify-center flex flex-col items-center text-center shadow-[0_8px_30px_rgb(0,0,0,0.12)] transition-all ease-in-out duration-800 hover:scale-105  rounded-lg">
+        <div class=" h-96 hover:bg-gradient-to-b justify-center flex flex-col items-center text-center shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:shadow-[0px_0px_20px_13px_#00000024] transition-all ease-in-out duration-800 hover:scale-105  rounded-3xl">
           <img alt="team" class="flex-shrink-0 rounded-full  h-60 object-cover object-center p-5 mb-3" src={items.img} />
           <div class="w-full">
             <h1 class="title-font text-xl text-gray-900 font-poppins font-bold">{items.name}</h1>
