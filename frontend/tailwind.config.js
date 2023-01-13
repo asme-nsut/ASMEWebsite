abc70f3fbe3097ca2bef32a846482c2665b2c8a4
Pulkit Batra <95132680+PulkitBxtra@users.noreply.github.com>
2023-01-14 00:31:48 +0530

    Revert "Revert "Gallery""

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        appear: {
+          '0%': { opacity:'0' },
+          '100%': { opacity:'1' },
+        }
+      },
+      animation: {
+        appear: 'appear 2s ease-in-out 1',
+      },
+    },
   },
   plugins: [],
 }
