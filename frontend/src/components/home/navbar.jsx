da91e5fcb29ceab5dda4d281ba2bc30dda786813
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-01-15 00:03:53 +0530

    added contact page

--- frontend/src/components/home/navbar.jsx
+++ frontend/src/components/home/navbar.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 function Nav() {
   let [open, setOpen] = useState(true);
 
+  
+
   return (
     <div>
       <header class="text-gray-600 body-font ">
@@ -63,7 +65,7 @@ function Nav() {
               Gallery
             </a>
           </nav>
-          <button class="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 md:visible ">
+          <button class="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 md:visible " >
             Contact
             <svg
               fill="none"
@@ -91,6 +93,8 @@ function Nav() {
       </header>
     </div>
   );
+
+  
 }
 
 export default Nav;
