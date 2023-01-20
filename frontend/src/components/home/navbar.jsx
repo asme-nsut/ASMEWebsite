9a4ddd133d8826ccda30f98302e386df15536687
Dhruv <dhruvdeora31@gmail.com>
2023-01-20 21:08:23 +0530

    fixed mih h

--- frontend/src/components/home/navbar.jsx
+++ frontend/src/components/home/navbar.jsx
@@ -3,7 +3,7 @@ import "./home.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 function Nav() {
   let [open, setOpen] = useState(true);
@@ -13,16 +13,23 @@ function Nav() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_rtx10be', 'template_j4mb7at', form.current, 'SmfdSxlZE-q5n5a1t')
+    emailjs
+      .sendForm(
+        "service_rtx10be",
+        "template_j4mb7at",
+        form.current,
+        "SmfdSxlZE-q5n5a1t"
+      )
 
-      .then((result) => {
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
-
-  
 
   return (
     <div>
@@ -32,31 +39,41 @@ function Nav() {
             class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
             href="/"
           >
-            <img class="w-24 h-16 mt-2" src="https://i.imgur.com/Os73S9e.png" alt="asme logo"/>
+            <img
+              class="w-24 h-16 mt-2"
+              src="https://i.imgur.com/Os73S9e.png"
+              alt="asme logo"
+            />
           </a>
 
           <div class={`${open ? "hidden" : "md:hidden"}`}>
             <div class="flex flex-col mr-1">
-
-            <hr></hr>
+              <hr></hr>
               <a class="mr-5 text-xl text-center hover:text-[#0C397D]" href="/">
                 Home
               </a>
-              <a class="mr-5 text-xl text-center hover:text-[#0C397D]" href="/about">
+              <a
+                class="mr-5 text-xl text-center hover:text-[#0C397D]"
+                href="/about"
+              >
                 About
               </a>
-              <a class="mr-5 text-xl text-center hover:text-[#0C397D]" href="/gallery">
+              <a
+                class="mr-5 text-xl text-center hover:text-[#0C397D]"
+                href="/gallery"
+              >
                 Gallery
               </a>
 
-              <a class="mr-5 text-xl text-center hover:text-[#0C397D]" href="/Contact">
+              <a
+                class="mr-5 text-xl text-center hover:text-[#0C397D]"
+                href="/Contact"
+              >
                 Contact
-                
               </a>
-              
             </div>
 
-          <hr></hr>
+            <hr></hr>
 
             <div>
               <button class="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 md:visible ">
@@ -88,20 +105,20 @@ function Nav() {
             </a>
           </nav>
           <a href="/contact" class="hidden md:inline-flex items-center">
-          <button class="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 md:visible " >
-            Contact
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+            <button class="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 md:visible ">
+              Contact
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
           </a>
 
           <div
@@ -117,8 +134,6 @@ function Nav() {
       </header>
     </div>
   );
-
-  
 }
 
 export default Nav;
