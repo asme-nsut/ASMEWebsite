7457d83eb514619b7e94025441b46f880f03a290
Chirag <chiragsaini6405@gmail.com>
2023-01-27 18:45:14 +0530

    final

--- frontend/src/components/events/eventdiv.jsx
+++ frontend/src/components/events/eventdiv.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function eventDiv({events,eventid}) {
   return (
     <>
-    <div class="py-8 px-4 lg:w-[45%] mx-4 my-4 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]">
+    <div class="py-8 px-4 lg:w-[45%] mx-4 my-4 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
         <div class="h-full flex items-start ">
           <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
           <img alt="team" class="flex-shrink-0 lg:p-0 rounded-full w-15 h-15 object-cover object-center sm:mb-0" src={events.logo} />
