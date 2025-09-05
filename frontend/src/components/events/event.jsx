4645a7d762912d470564f4904907d5b23e6dd35e
Yash2402 <yashsharma4583@gmail.com>
2025-09-06 00:28:24 +0530

    moved competitions to seperate page and added more events

--- frontend/src/components/events/event.jsx
+++ frontend/src/components/events/event.jsx
@@ -1,91 +1,106 @@
-import React from 'react';
 import EventDiv from './eventdiv';
 
-const iconPath = process.env.PUBLIC_URL + '/abimages/';
-
+const iconPath = process.env.PUBLIC_URL + '/events/';
 function Event() {
-  const competitions = [
-    {
-      mainTitle: 'ASME India EFx 2025',
-      subEvents: [
+    const images = [
+        '1_beyond_the _grid.jpg',
+        '2_green_business_pitch.jpg',
+        '3_bridge_the_gap.jpg',
+        '4_engineering_legends.jpg',
+        '5_auto_expo.jpg',
+        '6_mechie_mondays.png',
+        '7_moon_day.jpg',
+        '8_tower_rush.jpg',
+        '9_3d_modelling_challenge.jpg'
+    ];
+    const events = [
         {
-          name: 'ASME IAM3D Drone Competition',
-          brief: 'Team Mavericks ',
-          desc: 'ASME NSUT’s Team Mavericks participated in the ASME Innovative Additive Manufacturing 3D (IAM3D) Competition during ASME EFx 2025, held at Reva University, Bangalore. The challenge tasked student teams to design and manufacture an Unmanned Aerial Racing Cargo Vehicle (U.A.R.C.V.) using additive manufacturing and an iterative design process. The vehicle had to race through an obstacle course while picking up and delivering payloads each lap, testing both innovation and engineering skills With their innovative design and exceptional performance, Team Mavericks secured an impressive All India Rank (AIR) 7, underscoring ASME NSUT’s spirit of innovation, technical excellence, and problem-solving on a national platform',
-          logo: iconPath + 'iam3d_logo.png',
-        },
-        {
-          name: 'ASME Elevator Pitch Competition',
-          
-          desc: 'During ASME India EFx, ASME NSUT participated in the Elevator Pitch Competition, where participants had to deliver concise and persuasive speeches to spark interest in their ideas. Representing the society, Aman Malhotra showcased outstanding communication and presentation skills, securing 3rd position and highlighting the team’s ability to convey impactful ideas with clarity and confidence.',
-          logo: iconPath + 'elevator_logo.png',
-        },
-        {
-          name: 'ASME Oral Competition',
-          desc: 'Oral Competition at EFx India 2025 was a platform where participants showcased their innovative ideas, research, and projects through impactful speeches and presentations. ASME NSUT actively participated in the competition, demonstrating clarity, creativity, and technical depth, while proudly representing NSUT at the national stage.',
-          logo: iconPath + 'oral_comp_logo.png',
-        },
-        
-      ],
-    },
-    {
-      mainTitle: 'ASME E-Fest 2025',
-      subEvents: [
-        {
-          name: 'ASME Extended Reality Challenge – Autonomous Vehicles',
-          brief: 'Team Zeus',
-          desc: 'ASME NSUT’s Team Zeus participated in the Autonomous Vehicle – Extended Reality Challenge at ASME E-Fest 2025. The competition challenged students to design and simulate autonomous vehicle solutions within an extended reality environment using HyperSkill software, testing their ability to integrate design principles, control strategies, and innovation in virtual settings.',
-          logo: iconPath + 'xrc_autoveh_logo.png',
-        },
-        {
-          name: 'ASME Extended Reality Challenge – Lunar Landers',
-          brief: 'Team Phoenix',
-          desc: 'ASME NSUT’s Team Phoenix participated in the Lunar Lander – Extended Reality Challenge at ASME E-Fest 2025. This futuristic competition tasked students with designing and simulating a lunar landing mission within an extended reality environment using HyperSkill software, testing precision, problem-solving, and engineering design under challenging space exploration scenarios.',
-          logo: iconPath + 'xrc_lunar_lander_logo.png',
-        },
-        {
-          name: 'E-Fest Scavenger Hunt',
-          desc: 'ASME NSUT students enthusiastically participated in the Scavenger Hunt at ASME E-Fest 2025, a fun and interactive challenge designed to test teamwork, problem-solving, and quick thinking. The fostered collaboration, creativity, and the spirit of networking in a high-energy environment.',
-          logo: iconPath + 'scavenger_hunt logo.png',
-        },
-        {
-          name: 'ASME Technical Digital Poster Competition',
-          brief: '',
-          desc: 'ASME NSUT participated in the Old Guard Technical Digital Poster Competition at ASME E-Fest Tech Connect 2025, showcasing innovative technical ideas through clear and visually engaging posters. The team highlighted creativity, engineering knowledge, and effective communication while representing ASME NSUT on the national stage.',
-          logo: iconPath + 'OldGuardPosterComp_logo.png',
-        },
-      ],
-    },
-  ];
+            mainTitle: 'Chapter Events',
+            subEvents: [
+                {
+                    name: 'Beyond The Grid',
+                    brief: '',
+                    desc: 'Beyond The Grid was a 3D design competition organized by ASME NSUT, bringing together enthusiasts from various colleges to showcase their creativity and technical skills. Powered by Morris Garages, participants solved real-life design problems within a set time frame using 3D software, blending innovation with practical application.',
+                    logo: iconPath + images[0],
+                },
+                {
+                    name: 'Green Business Pitch',
+                    brief: '',
+                    desc: 'Green Business Pitch was an ideathon competition organised by ASME NSUT aligned with the Sustainable Development Goals, where participants presented their business ideas to a panel of judges. The event featured engaging presentations and an interactive Q&A session, encouraging creativity, collaboration, and awareness of sustainable business practices.',
+                    logo: iconPath + images[1],
+                },
+                {
+                    name: 'Bridge the Gap',
+                    brief: '',
+                    desc: 'Bridge the Gap was a creative and interactive event by ASME NSUT where participants designed and built bridges using only ice cream sticks and glue. The competition tested both creativity and engineering logic, as bridges were evaluated based on their strength and ability to withstand added weight, making it an exciting blend of innovation and friendly competition.',
+                    logo: iconPath + images[2],
+                },
+                {
+                    name: 'Engineering Legends',
+                    brief: '',
+                    desc: 'Engineering Legends was an engaging event by ASME NSUT, featuring a hands-on drone-making workshop followed by a thrilling quiz. Organized exclusively for women, it aimed to promote Women in Engineering and introduce participants to ASME’s IAM3D Competition. The event combined innovation, practical learning, and teamwork, inspiring attendees with skills in drone technology and design.',
+                    logo: iconPath + images[3],
+                },
+                {
+                    name: 'ASME NSUT x AutoExpo 2025',
+                    brief: '',
+                    desc: 'ASME NSUT x AutoExpo 2025 was an industrial visit organized to give newly recruited members exposure to the latest trends and innovations in the automobile industry. Held at AutoExpo 2025 in Pragati Maidan, the event showcased cutting-edge technologies from leading companies and highlighted sustainable advancements in mobility. Members also experienced live demonstrations, including thrilling roadshows by premium automobile brands.',
+                    logo: iconPath + images[4],
+                },
+                {
+                    name: 'Mechie Mondays',
+                    brief: '',
+                    desc: 'Mechie Mondays was a weekly blog series by ASME NSUT, published on Instagram to spark curiosity and share knowledge in mechanical engineering. Each Monday featured fresh insights, industry trends, innovative ideas, and engaging stories, making it a go-to series for students and enthusiasts alike.',
+                    logo: iconPath + images[5],
+                },
+                {
+                    name: 'Online Workshop on National Moon Day',
+                    brief: '',
+                    desc: 'ASME NSUT organized a National Moon Day online workshop to mark the historic first steps on the Moon. The session explored the working of lunar landers through CAD simulations and engineering insights, engaging participants with space science and design concepts.',
+                    logo: iconPath + images[6],
+                },
+                {
+                    name: 'Tower Rush',
+                    brief: '',
+                    desc: 'Tower Rush was a fun and engaging event under NSUTTHON, filled with excitement and friendly competition. Participants, divided into teams, had to build a pyramid of cups inside a circle while following specific rules. The team that successfully completed the pyramid first was declared the winner.',
+                    logo: iconPath + images[7],
+                },
+                {
+                    name: '3D Modeling Challenge',
+                    brief: '',
+                    desc: 'The 3D Modeling Challenge was organized by ASME NSUT, where students showcased their creativity and CAD skills by solving real-world problems or recreating given models, blending innovation with technical expertise.',
+                    logo: iconPath + images[8],
+                }
+            ]
+        }
+    ];
 
-  return (
-    <div>
-      <section className="text-gray-600 body-font py-10">
-        <div className="text-center ">
-          <h1 className="sm:text-3xl text-2xl title-font text-gray-900 mb-4 font-poppins font-black">
-            Competitions
-          </h1>
-          <div className="flex mt-6 justify-center">
-            <div className="w-40 h-1 rounded-full bg-indigo-500 inline-flex"></div>
-          </div>
+    return (
+        <div>
+            <section className="text-gray-600 body-font">
+                <div className="text-center my-10 flex flex-col space-y-4">
+                    <h1 className="text-7xl font-semibold text-gray-900">Events</h1>
+                    <div className="flex justify-center">
+                        <div className="w-60 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                    </div>
+                </div>
+
+                {events.map((competition, idx) => (
+                    <div key={idx} className="container px-5 py-24 mx-auto">
+                        <h2 className="text-center text-3xl font-black mb-8 font-poppins text-[#0C397D] tracking-wide">
+                            {competition.mainTitle}
+                        </h2>
+
+                        <div className="flex flex-wrap -mx-4 -my-8 justify-center">
+                            {competition.subEvents.map((subEvent) => (
+                                <EventDiv event={subEvent}/>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </section>
         </div>
-
-        {competitions.map((competition, idx) => (
-          <div key={idx} className="container px-5 py-24 mx-auto">
-            <h2 className="text-center text-3xl font-black mb-8 font-poppins text-[#0C397D] tracking-wide">
-            {competition.mainTitle}
-            </h2>
-
-            <div className="flex flex-wrap -mx-4 -my-8 justify-center">
-              {competition.subEvents.map((subEvent, subIdx) => (
-                <EventDiv key={subIdx} events={subEvent} eventid={subIdx} />
-              ))}
-            </div>
-          </div>
-        ))}
-      </section>
-    </div>
-  );
+    );
 }
 
 export default Event;
--- frontend/src/components/events/eventdiv.jsx
+++ frontend/src/components/events/eventdiv.jsx
@@ -1,31 +1,20 @@
-import React from 'react'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-
-function eventDiv({events,eventid}) {
-  return (
-    <>
-    <div class="py-8 px-4 lg:w-[45%] mx-4 my-4 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
-        <div class="h-full flex items-start ">
-          <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
-          <img alt="team" class="flex-shrink-0 lg:p-0 rounded-full w-15 h-15 object-cover object-center sm:mb-0" src={events.logo} />
-          </div>
-          <div class="flex-grow pl-6">
-            <h1 class="title-font text-2xl text-gray-900 mb-1 font-poppins font-bold">{events.name}</h1>
-            <h2 class="tracking-widest md:text-sm title-font font-medium text-indigo-500 mb-2 font-poppins">{events.brief}</h2>
-            <p class="leading-relaxed mb-5 md:text-lg font-poppins font-medium">{events.desc}</p>
-            {/* <a class="inline-flex items-center">
-              <img alt="blog" src="https://dummyimage.com/103x103" class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center" />
-              <span class="flex-grow flex flex-col pl-3">
-                <span class="title-font font-medium text-gray-900">Alper Kamu</span>
-              </span>
-            </a> */}
-          </div>
-        </div>
-    </div>
-    </>
-  )
+function eventDiv({ event }) {
+    return (
+        <>
+            <div class="py-8 px-4 lg:w-[45%] mx-4 my-4 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+                <div class="flex flex-col items-center -mt-4">
+                    <div class="flex-shrink-0 h-[400px] mb-5 rounded-[15px] object-contain overflow-hidden flex flex-col text-center leading-none">
+                        <img alt={event.logo} src={event.logo} />
+                    </div>
+                    <div class="flex-grow pl-6">
+                        <h1 class="title-font text-2xl text-gray-900 mb-1 font-poppins font-bold">{event.name}</h1>
+                        <h2 class="tracking-widest md:text-sm title-font font-medium text-indigo-500 mb-2 font-poppins">{event.brief}</h2>
+                        <p class="leading-relaxed mb-5 md:text-lg font-poppins font-medium">{event.desc}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default eventDiv;
