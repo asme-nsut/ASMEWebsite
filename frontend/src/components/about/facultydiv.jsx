7f3165807fa3e9110f1ecb9f167c1d495ddc09bc
Yash2402 <yashsharma4583@gmail.com>
2025-09-06 12:31:28 +0530

    removed: redundant imports

--- frontend/src/components/about/facultydiv.jsx
+++ frontend/src/components/about/facultydiv.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import AOS from 'aos';
+// import { useEffect } from 'react';
+// import AOS from 'aos';
 // import 'aos/dist/aos.css';
 
 function FacultyDiv({ items, facultyId }) {
