de9f77ad3364575555c55f462979500fcb0aeb01
Yash2402 <yashsharma4583@gmail.com>
2025-09-06 12:19:52 +0530

    added: decode the decades and updated the images

--- frontend/src/components/events/event.jsx
+++ frontend/src/components/events/event.jsx
@@ -3,7 +3,7 @@ import EventDiv from './eventdiv';
 const iconPath = process.env.PUBLIC_URL + '/events/';
 function Event() {
     const images = [
-        '1_beyond_the _grid.jpg',
+        '1_beyond_the_grid.jpg',
         '2_green_business_pitch.jpg',
         '3_bridge_the_gap.jpg',
         '4_engineering_legends.jpg',
@@ -11,7 +11,8 @@ function Event() {
         '6_mechie_mondays.png',
         '7_moon_day.jpg',
         '8_tower_rush.jpg',
-        '9_3d_modelling_challenge.jpg'
+        '9_decode_the_decades.jpg',
+        '10_3d_modelling_challenge.jpg'
     ];
     const events = [
         {
@@ -19,57 +20,53 @@ function Event() {
             subEvents: [
                 {
                     name: 'Beyond The Grid',
-                    brief: '',
                     desc: 'Beyond The Grid was a 3D design competition organized by ASME NSUT, bringing together enthusiasts from various colleges to showcase their creativity and technical skills. Powered by Morris Garages, participants solved real-life design problems within a set time frame using 3D software, blending innovation with practical application.',
                     logo: iconPath + images[0],
                 },
                 {
                     name: 'Green Business Pitch',
-                    brief: '',
                     desc: 'Green Business Pitch was an ideathon competition organised by ASME NSUT aligned with the Sustainable Development Goals, where participants presented their business ideas to a panel of judges. The event featured engaging presentations and an interactive Q&A session, encouraging creativity, collaboration, and awareness of sustainable business practices.',
                     logo: iconPath + images[1],
                 },
                 {
                     name: 'Bridge the Gap',
-                    brief: '',
                     desc: 'Bridge the Gap was a creative and interactive event by ASME NSUT where participants designed and built bridges using only ice cream sticks and glue. The competition tested both creativity and engineering logic, as bridges were evaluated based on their strength and ability to withstand added weight, making it an exciting blend of innovation and friendly competition.',
                     logo: iconPath + images[2],
                 },
                 {
                     name: 'Engineering Legends',
-                    brief: '',
                     desc: 'Engineering Legends was an engaging event by ASME NSUT, featuring a hands-on drone-making workshop followed by a thrilling quiz. Organized exclusively for women, it aimed to promote Women in Engineering and introduce participants to ASME’s IAM3D Competition. The event combined innovation, practical learning, and teamwork, inspiring attendees with skills in drone technology and design.',
                     logo: iconPath + images[3],
                 },
                 {
                     name: 'ASME NSUT x AutoExpo 2025',
-                    brief: '',
                     desc: 'ASME NSUT x AutoExpo 2025 was an industrial visit organized to give newly recruited members exposure to the latest trends and innovations in the automobile industry. Held at AutoExpo 2025 in Pragati Maidan, the event showcased cutting-edge technologies from leading companies and highlighted sustainable advancements in mobility. Members also experienced live demonstrations, including thrilling roadshows by premium automobile brands.',
                     logo: iconPath + images[4],
                 },
                 {
                     name: 'Mechie Mondays',
-                    brief: '',
                     desc: 'Mechie Mondays was a weekly blog series by ASME NSUT, published on Instagram to spark curiosity and share knowledge in mechanical engineering. Each Monday featured fresh insights, industry trends, innovative ideas, and engaging stories, making it a go-to series for students and enthusiasts alike.',
                     logo: iconPath + images[5],
                 },
                 {
                     name: 'Online Workshop on National Moon Day',
-                    brief: '',
                     desc: 'ASME NSUT organized a National Moon Day online workshop to mark the historic first steps on the Moon. The session explored the working of lunar landers through CAD simulations and engineering insights, engaging participants with space science and design concepts.',
                     logo: iconPath + images[6],
                 },
                 {
                     name: 'Tower Rush',
-                    brief: '',
                     desc: 'Tower Rush was a fun and engaging event under NSUTTHON, filled with excitement and friendly competition. Participants, divided into teams, had to build a pyramid of cups inside a circle while following specific rules. The team that successfully completed the pyramid first was declared the winner.',
                     logo: iconPath + images[7],
                 },
                 {
-                    name: '3D Modeling Challenge',
-                    brief: '',
-                    desc: 'The 3D Modeling Challenge was organized by ASME NSUT, where students showcased their creativity and CAD skills by solving real-world problems or recreating given models, blending innovation with technical expertise.',
+                    name: 'Decode the Decades',
+                    desc: 'ASME NSUT’s Decode the Decades at Moksha brought out the fun side of teamwork as participants cracked cartoon-themed puzzles and challenges. The event combined nostalgia, energy, and collaboration.',
                     logo: iconPath + images[8],
+                },
+                {
+                    name: '3D Modeling Challenge',
+                    desc: 'The 3D Modeling Challenge was organized by ASME NSUT, where students showcased their creativity and CAD skills by solving real-world problems or recreating given models, blending innovation with technical expertise.',
+                    logo: iconPath + images[9],
                 }
             ]
         }
@@ -77,7 +74,7 @@ function Event() {
 
     return (
         <div>
-            <section className="text-gray-600 body-font">
+            <section className="text-gray-600 body-font mb-10">
                 <div className="text-center my-10 flex flex-col space-y-4">
                     <h1 className="text-7xl font-semibold text-gray-900">Events</h1>
                     <div className="flex justify-center">
@@ -86,14 +83,16 @@ function Event() {
                 </div>
 
                 {events.map((competition, idx) => (
-                    <div key={idx} className="container px-5 py-24 mx-auto">
-                        <h2 className="text-center text-3xl font-black mb-8 font-poppins text-[#0C397D] tracking-wide">
-                            {competition.mainTitle}
-                        </h2>
+                    <div key={idx} className="container mx-auto">
+                        {
+                            // <h2 className="text-center text-3xl font-black font-poppins text-[#0C397D] tracking-wide">
+                            // {competition.mainTitle}
+                            // </h2>
+                        }
 
-                        <div className="flex flex-wrap -mx-4 -my-8 justify-center">
+                        <div className="flex flex-wrap justify-center">
                             {competition.subEvents.map((subEvent) => (
-                                <EventDiv event={subEvent}/>
+                                <EventDiv event={subEvent} />
                             ))}
                         </div>
                     </div>
--- frontend/src/components/events/eventdiv.jsx
+++ frontend/src/components/events/eventdiv.jsx
@@ -1,15 +1,15 @@
 function eventDiv({ event }) {
     return (
         <>
-            <div class="py-8 px-4 lg:w-[45%] mx-4 my-4 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
-                <div class="flex flex-col items-center -mt-4">
-                    <div class="flex-shrink-0 h-[400px] mb-5 rounded-[15px] object-contain overflow-hidden flex flex-col text-center leading-none">
+            <div class="lg:w-[45%] m-3 shadow-[4px_6px_16px_3px_#00000024] rounded-[25px]" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+                <div class="flex flex-col items-start space-y-4 sm:space-y-0 sm:flex-row p-6 sm:space-x-7">
+                    <div class="flex-shrink-0 sm:h-[200px] sm:w-[200px] rounded-[5px] object-contain overflow-hidden">
                         <img alt={event.logo} src={event.logo} />
                     </div>
-                    <div class="flex-grow pl-6">
-                        <h1 class="title-font text-2xl text-gray-900 mb-1 font-poppins font-bold">{event.name}</h1>
-                        <h2 class="tracking-widest md:text-sm title-font font-medium text-indigo-500 mb-2 font-poppins">{event.brief}</h2>
-                        <p class="leading-relaxed mb-5 md:text-lg font-poppins font-medium">{event.desc}</p>
+                    <div class="flex-grow flex-col space-y-3">
+                        <h1 class="title-font text-2xl text-gray-900 font-poppins font-bold">{event.name}</h1>
+                        { /* <h2 class="tracking-widest md:text-sm title-font font-medium text-indigo-500 mb-2 font-poppins">{event.brief}</h2> */}
+                        <p class="leading-relaxed md:text-lg font-poppins font-medium">{event.desc}</p>
                     </div>
                 </div>
             </div>
