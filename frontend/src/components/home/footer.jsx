4e5eaf30cdba44591816186efbffad7b2148369b
Yash2402 <yashsharma4583@gmail.com>
2025-09-06 18:24:45 +0530

    added: Alumni under Links section

--- frontend/src/components/home/footer.jsx
+++ frontend/src/components/home/footer.jsx
@@ -47,6 +47,9 @@ function footer() {
                                     <a class="text-gray-600 hover:text-gray-800" href="/gallery">Gallery</a>
                                 </li>
                                 <li>
+                                    <a class="text-gray-600 hover:text-gray-800" href="/alumni">Alumni</a>
+                                </li>
+                                <li>
                                     <a class="text-gray-600 hover:text-gray-800" href="/contact">Contact</a>
                                 </li>
                             </nav>
@@ -134,13 +137,7 @@ function footer() {
                             <a href="https://www.asmesut.com/" class="text-gray-600 ml-1">ASME</a>
                         </p>
                         <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                            <a class="text-gray-500" href="https://www.facebook.com/DevcommNsut">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                </svg>
-                            </a>
-
-                            <a class="ml-3 text-gray-500" href="https://www.instagram.com/devcomm.nsut/">
+                            <a class="ml-3 text-gray-500" href="https://www.instagram.com/asme.nsut/">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                     <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                                     <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
